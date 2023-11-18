98d4b2125f1d864b66be5d4210688ce2f25ae5a2
Your Name <you@example.com>
2023-11-18 00:51:43 +0000

    init

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable instance_type {
+  type        = string
+  default     = ""
+  description = "instance type"
+}
+
+variable instance_name {
+  type        = string
+  default     = ""
+  description = "instance name"
+}
